101d97e4dc921ff9315a82153fac1cb5f145ad04
hammad-PC\hammad <khanansaridon@gmail.com>
2021-05-07 11:45:29 +0500

    assin

---
+++ main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(hi());
+}
+
+class hi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: todo
+    // TODO: implement build
+    return MaterialApp(
+        home: Scaffold(
+      body: Text("Hello hammad"),
+    )
+        // ignore: dead_code
+        );
+  }
+}
